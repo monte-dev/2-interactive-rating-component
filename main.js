46c88161a6cdcf24a5a928d99a2b22bef511f99a
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-01-01 21:33:05 +0000

    Added comments to revise the javascript code used to select a button and check its state

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+// select all elements with a class .btn in the HTML, these are stored in a NodeList object ~ similar to array
 const ratingBtn = document.querySelectorAll(".btn");
-
+// Initialize previousBtn as undefined this will be used to track previously clicked button
+let previousBtn;
+// using forEach loop to go through items in ratingBtn NodeList(array), takes callback function as an argument, callback function uses argument btn to select item from NodeList
 ratingBtn.forEach((btn) => {
+// this line adds a click event listener to current .btn
   btn.addEventListener("click", () => {
+// this line checks if previousBtn is defined, and if so removes a class .clicked from it
+    if (previousBtn) {
+        previousBtn.classList.remove('clicked')
+    }
+    // This line adds .clicked class to current .btn  and assigns it to previous button to track the state for next iteration of the loop
     btn.classList.add("clicked");
+    previousBtn = btn;
   });
 });
