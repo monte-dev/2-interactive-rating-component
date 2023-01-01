73ad7d111b871d055b15694a743e8734c9c45c38
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-01-01 22:31:42 +0000

    Added full functionality to display thank you state with user's selection

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ const ratingBtn = document.querySelectorAll(".btn");
 let previousBtn;
 // using forEach loop to go through items in ratingBtn NodeList(array), takes callback function as an argument, callback function uses argument btn to select item from NodeList
 ratingBtn.forEach((btn) => {
-// this line adds a click event listener to current .btn
+  // this line adds a click event listener to current .btn
   btn.addEventListener("click", () => {
-// this line checks if previousBtn is defined, and if so removes a class .clicked from it
+    // this line checks if previousBtn is defined, and if so removes a class .clicked from it
     if (previousBtn) {
-        previousBtn.classList.remove('clicked')
+      previousBtn.classList.remove("clicked");
     }
     // This line adds .clicked class to current .btn  and assigns it to previous button to track the state for next iteration of the loop
     btn.classList.add("clicked");
     previousBtn = btn;
+// assign a value of clicked btn to ratingValue variable
+    let ratingValue = btn.innerHTML;
+    const submitBtn = document.querySelector("#submit");
+
+    submitBtn.addEventListener("click", () => {
+      const thankYouState = document.querySelector(".thank-you");
+      const ratingState = document.querySelector(".rating");
+      const ratingResult = document.querySelector(".rating-result");
+    //   hide and show different states
+      ratingState.style.display = "none";
+      thankYouState.style.display = "flex";
+// fill in the text of paragraph with users selected rating using template literal
+      ratingResult.innerHTML = `You selected ${ratingValue} out of 5`;
+    });
   });
 });
